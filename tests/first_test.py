3abab1810febf988db0e9d2a5e598d27e495641a
gbielanova <gbielanova@gmail.com>
2026-09-24 14:55:06 +0300

    Refactored tests

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,29 +1,27 @@
+import uuid
 from time import sleep
 
 from playwright.sync_api import Page, expect
 
 
-def test_login(page: Page):
-    page.goto('https://test.yoy.events/')
-    expect(page).to_have_title('йой! — події в твоєму місті ')
+def test_register_a_user(page: Page):
+    email = generate_unique_email()
 
-    page.locator('a[href="/me"]').click()
+    navigate_to_page(page, 'me')
 
     expect(page.locator('[data-testid="signin-returntohint"]')).to_have_text('Після входу повернемо тебе назад.')
 
-    page.locator('[data-testid="signin-email-input"]').fill('test1@test.com')
-    page.locator('[data-testid="signin-otp-submit-label"]').click()
-    page.locator('[data-testid="signin-code-input"]').fill('000000')
+    fill_login_form(page, email)
 
-    page.locator('[data-testid="signin-code-submit-label"]').click()
+    check_me_user_info(page, email, "")
 
-    expect(page.locator('[data-testid="me-display-name"]')).to_have_text('test1@test.com')
-
-    page.locator('data-testid="logout-btn"').click()
+    logout(page)
 
 
 def test_register_to_event(page: Page):
-    page.goto('https://test.yoy.events/')
+    email = generate_unique_email()
+
+    navigate_to_page(page)
 
     conf_name = page.locator('[data-event-card] h3').first.inner_text()
 
@@ -33,22 +31,57 @@ def test_register_to_event(page: Page):
 
     page.locator('[data-testid="register-cta"]').click()
 
-    page.locator('#reg_name').fill('Іван')
-    page.locator('#reg_lastname').fill('Петренко')
-    page.locator('#reg_email').fill('test9@test.com')
-    page.locator('#reg_phone').fill('+380000000000')
-
-    page.locator('#sheet_submit_btn').click()
+    fill_event_register_form(email, page)
 
     expect(page.locator('#sheet_confirm_view h3')).to_have_text("Готово! Твій квиток")
     expect(page.locator('[data-testid="ticket-event-title"]')).to_have_text(conf_name)
 
-    # it takes some time to regiter a user, playwright is too fast clicking on the link
+    # it takes some time to register a user, playwright is too fast clicking on the link
     sleep(1)
 
-    page.locator('[href="/me"]').click()
+    go_to_me_page(page)
+    check_me_user_info(page, email, 'Іван Петренко')
+    logout(page)
 
-    expect(page.locator('[data-testid="me-display-name"]')).to_have_text('Іван Петренко')
-    expect(page.locator('[data-testid="me-display-name"] + div')).to_have_text('test9@test.com')
+
+def fill_event_register_form(email: str, page: Page):
+    page.locator('#reg_name').fill('Іван')
+    page.locator('#reg_lastname').fill('Петренко')
+    page.locator('#reg_email').fill(email)
+    page.locator('#reg_phone').fill('+380000000000')
+    page.locator('#sheet_submit_btn').click()
 
 
+def check_me_user_info(page: Page, email: str, name: str):
+    if name == '':
+        expect(page.locator('[data-testid="me-display-name"]')).to_have_text(email)
+    else:
+        expect(page.locator('[data-testid="me-display-name"]')).to_have_text(name)
+        expect(page.locator('[data-testid="me-display-name"] + div')).to_have_text(email)
+
+
+def navigate_to_page(page: Page, url=''):
+    page.goto('https://test.yoy.events/'+ url)
+
+
+def logout(page: Page):
+    page.locator('[data-testid="logout-btn"]').click()
+
+
+def go_to_me_page(page: Page):
+    page.locator('a[href="/me"]').click()
+
+
+def generate_unique_email(prefix: str = "gb_test", domain: str = "example.com") -> str:
+    return f"{prefix}_{uuid.uuid4().hex[:8]}@{domain}"
+
+
+def fill_login_form(page: Page, email: str):
+    page.locator('[data-testid="signin-email-input"]').fill(email)
+    page.locator('[data-testid="signin-otp-submit-label"]').click()
+
+    # code validation will be failed if user wasn't reated yet
+    sleep(1)
+
+    page.locator('[data-testid="signin-code-input"]').fill('000000')
+    page.locator('[data-testid="signin-code-submit-label"]').click()
