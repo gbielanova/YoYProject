9e8d51435981e3030c3a955b28c800128c8a9cc9
gbielanova <gbielanova@gmail.com>
2026-09-25 14:42:01 +0300

    MNFCTR-2-module: rolled back test

--- tests/first_test.py
+++ tests/first_test.py
@@ -21,6 +21,20 @@ def test_register_a_user(page: Page, configs: Config):
     logout(page)
 
 
+def test_register_a_user_with_invalid_code(page: Page, configs: Config):
+    email = generate_unique_email(configs.domain)
+
+    navigate_to_page(page, configs.base_url, 'me')
+
+    expect(page.locator('[data-testid="signin-returntohint"]')).to_have_text('Після входу повернемо тебе назад.')
+
+    fake = Faker("en_US")
+    code = fake.numerify('######')
+
+    fill_login_form(page, email, code=code)
+
+    expect(page.locator('[data-testid="signin-new-code-msg"]')).to_have_text("Код недійсний або прострочений. Запроси новий код.")
+
 def test_register_to_event(page: Page, configs: Config):
     email = generate_unique_email(configs.domain)
     fake = Faker()
